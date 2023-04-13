70c4aae21f934edcee33f65b0df65be99a95c531
JavierCas35 <javiercas25@gmail.com>
2023-04-13 17:36:14 +0200

    Deployment de la app

---
+++ views/auth/confirmar-cuenta.php
@@ -0,0 +1,10 @@
+
+
+<?php include_once __DIR__ . '/../templates/alertas.php'; ?>
+
+<h1 class="nombre-pagina">Confirmar Cuenta</h1>
+
+<div class="acciones">
+    <a href="/">Iniciar Sesion.</a>
+   
+</div>
